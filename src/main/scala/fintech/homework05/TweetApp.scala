96c0140f45c7454065dfd3ef02ee68f3cb686378
Nikita Kozhevnikov <NKtargeting@yandex.ru>
2018-10-22 17:04:47 +0500

    Minor fixes for error texts and formatting

--- src/main/scala/fintech/homework05/TweetApp.scala
+++ src/main/scala/fintech/homework05/TweetApp.scala
@@ -3,8 +3,6 @@ package fintech.homework05
 import java.time.Instant
 import java.util.UUID
 
-import com.sun.net.httpserver.Authenticator
-
 import scala.collection.immutable.HashMap
 
 /**
@@ -55,7 +53,7 @@ final class InMemoryTweetStorage() extends TweetStorage {
   private var storage: Map[String, Tweet] = new HashMap()
 
   def saveTweet(tweet: Tweet): Result[Tweet] = storage.get(tweet.id) match {
-    case Some(_) => Error(s"Tweet with id ${tweet.id} already exists in the storage!")
+    case Some(_) => Error(s"Storage save error: tweet with id ${tweet.id} already exists in the storage!")
     case None =>
       storage = storage.updated(tweet.id, tweet)
       Success(tweet)
@@ -63,17 +61,17 @@ final class InMemoryTweetStorage() extends TweetStorage {
 
   def loadTweet(id: String): Result[Tweet] = storage.get(id) match {
     case Some(tweet) => Success(tweet)
-    case None        => Error(s"Tweet with id $id not found in the storage!")
+    case None        => Error(s"Storage load error: tweet with id $id not found in the storage!")
   }
 
   /*
   * С точки зрения только лишь обновления лайков такой подход к обновлению твита менее эффективный, чем,
   * например, просто инкремент на единицу только в этом классе. Т.к. нам придётся в классе TweetApi сначала
   * запрашивать весь твит по id, чтобы узнать текущее количество лайков, а потом вызывать этот метод.
-  * Таким образом, мы открываем соединение с условной "базой данных" и ищем твит два раза вместо одного.
-  * Но при этом такой подход намного более расширяемый и гибкий, т.к. позволяет нам как обновлять другие
-  * члены класса Tweet, так и обновлять несколько полей за раз. В случае, если требования к классу в будущем
-  * изменятся, переписывать старый код и тесты не придётся.
+  * Таким образом, мы ищем твит в двух разных классах и открываем соединение с условной "базой данных" два раза
+  * вместо одного. Но при этом такой подход намного более расширяемый и гибкий, т.к. позволяет нам как
+  * обновлять другие члены класса Tweet, так и обновлять несколько полей за раз. В случае, если требования
+  * к классу в будущем изменятся, переписывать старый код и тесты не придётся.
   */
   def updateTweet(id:       String,
                   newUser:  Option[String] = None,
@@ -85,30 +83,30 @@ final class InMemoryTweetStorage() extends TweetStorage {
           case Some(text) => TweetApi.getHashTags(text)
           case None       => oldTweet.hashTags
         }
-        storage(id) = storage.updated(id, Tweet(oldTweet.id,
-                                                newUser.getOrElse(oldTweet.user),
-                                                newText.getOrElse(oldTweet.text),
-                                                newHashTagsIfNeeds,
-                                                oldTweet.createdAt,
-                                                newLikes.getOrElse(oldTweet.likes)))
+        storage = storage.updated(id, Tweet(oldTweet.id,
+                                            newUser.getOrElse(oldTweet.user),
+                                            newText.getOrElse(oldTweet.text),
+                                            newHashTagsIfNeeds,
+                                            oldTweet.createdAt,
+                                            newLikes.getOrElse(oldTweet.likes)))
         Success(storage(id))
 
       case None =>
-        Error(s"Tweet with id $id not found in the storage!")
+        Error(s"Storage update error: tweet with id $id not found in the storage!")
     }
 }
 
 class TweetApi(storage: TweetStorage) {
-  def createTweet(request: CreateTweetRequest): Result[Tweet] =
-    if (request.text.length <= 280)
-      storage.saveTweet(Tweet(UUID.randomUUID().toString,
-                              request.user,
-                              request.text,
-                              TweetApi.getHashTags(request.text),
-                              Some(Instant.now),
-                              0))
-    else
-      Error("Tweet has more than 280 symbols!")
+  def createTweet(request: CreateTweetRequest): Result[Tweet] = request match {
+    case CreateTweetRequest(text, _) if text.length > 280 => Error("API error: tweet has more than 280 symbols!")
+    // Другие проверки, если они понадобятся в будущем...
+    case CreateTweetRequest(text, user) => storage.saveTweet(Tweet(UUID.randomUUID().toString,
+                                                             user,
+                                                             text,
+                                                             TweetApi.getHashTags(text),
+                                                             Some(Instant.now),
+                                                             0))
+  }
 
   def getTweet(request: GetTweetRequest): Result[Tweet] =
     storage.loadTweet(request.id)
@@ -118,12 +116,10 @@ class TweetApi(storage: TweetStorage) {
       case Success(tweet) =>
         val newLikes = tweet.likes + 1
         storage.updateTweet(request.id, None, None, Some(newLikes)) match {
-          case Success(_)     => Success(newLikes)
-          case Error(message) => Error(s"Storage updating error: $message")
+          case Success(_) => Success(newLikes)
+          case Error(message) => Error(message)
         }
-
-      case Error(message) =>
-        Error(s"Storage finding error: $message")
+      case Error(message) => Error(message)
     }
 }
 
