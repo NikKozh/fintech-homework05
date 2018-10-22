1c657b3141e3416880ced3065f9b62da777dbcde
Nikita Kozhevnikov <NKtargeting@yandex.ru>
2018-10-22 14:04:41 +0500

    Add additional members to Result for tests

--- src/main/scala/fintech/homework05/Result.scala
+++ src/main/scala/fintech/homework05/Result.scala
@@ -1,11 +1,19 @@
 package fintech.homework05
 
 sealed trait Result[+T] {
+  def hasError: Boolean
+  def get: T // only for test
   override def toString: String = "Result"
 }
+
 final case class Success[T](value: T) extends Result[T] {
+  def hasError: Boolean = false
+  def get: T = value
   override def toString: String = super.toString + "[Success(" + value + ")]"
 }
+
 final case class Error[T](message: String) extends Result[T] {
+  def hasError: Boolean = true
+  def get: T = throw new NoSuchElementException
   override def toString: String = super.toString + "[Error(" + message + ")]"
 }
